c4ff8fe1d763e8d9e4c124cb725570895376b385
李伟国 <liweiguo1215@163.com>
2020-07-06 15:43:41 +0800

    添加分类实现

--- LWG_A_Category/Classes/A_Category/CTMediator+A.m
+++ LWG_A_Category/Classes/A_Category/CTMediator+A.m
@@ -10,4 +10,11 @@
 
 @implementation CTMediator (A)
 
+- (UIViewController *)A_aViewController{
+    /*
+     AViewController *viewController = [[AViewController alloc] init];
+     */
+    return [self performTarget:@"A" action:@"viewController" params:nil shouldCacheTarget:NO];
+}
+
 @end
